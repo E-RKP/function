cf3e97219c8508333afda575a1caa9a97b272b44
E-RKP <edobackup111@gmail.com>
2025-05-08 11:19:54 +0200

    version 2

---
+++ data.js
@@ -0,0 +1,32 @@
+let ciao = 'ciao'
+console.log(ciao)
+
+let rev = 10
+
+function triplicatore(){
+  for (i=0; i<3;i++){
+    rev = rev*3
+    console.log(rev)
+  }
+   
+}
+
+triplicatore();
+
+function quadriplicatore(){
+  for (i=0; i<4;i++){
+    rev=rev*4
+    console.log(rev)
+  }
+  console.log(rev)
+}
+
+quadriplicatore();
+
+let names ='cristina'
+
+function babygirl (nome){
+  console.log(nome)
+}
+
+babygirl(names);
