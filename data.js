9c3a4fd6748c3ab195d3f04d1f6a60defb7a309f
E-RKP <edobackup111@gmail.com>
2025-05-08 11:38:03 +0200

    added function

--- data.js
+++ data.js
@@ -30,3 +30,54 @@ function babygirl (nome){
 }
 
 babygirl(names);
+
+let soldi = 1000
+
+function stipendio (){
+
+  console.log(`Annuale: ${soldi*12}`)
+  console.log(`Mensile: ${soldi*1}`)
+}
+
+stipendio();
+
+
+function sottrazione(a,b){
+  console.log(a-b)
+}
+
+sottrazione(10,6);
+
+let m = prompt('inserisci visual')
+let c = prompt('inserisci categoria')
+let guadagno = 0
+function youtube(){
+
+  if (c == 'finanza'){
+    guadagno = (m/1000)*8
+    console.log(guadagno)
+  }else if (c == 'food'){
+    guadagno = (m/1000)*4
+    console.log(guadagno)
+  }else{
+    guadagno=(m/1000)*3
+    console.log(guadagno)
+  }
+
+}
+
+youtube();
+
+
+function yt(visual, categoria){
+
+  if (categoria == 'finanza'){
+    console.log((visual/1000)*8)
+  }else if(categoria=='food'){
+    console.log(visual/1000)
+  }else{
+    console.log(visual/1000)
+  }
+}
+
+yt(100000, 'finanza');
